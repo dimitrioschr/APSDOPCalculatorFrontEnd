c771c2101c8e4a7b50ff24a2ac0f4c17a650fc18
dimitrios <dimitrioschr@gmail.com>
2015-05-10 21:24:45 +0300

    all properly set in external classes

--- main.py
+++ main.py
@@ -17,11 +17,6 @@ class CPPopup(Popup):
     ballast_bonus_input_wid = ObjectProperty(None)
     aps_hire_rate_input_wid = ObjectProperty(None)
 
-    def Close(self):
-        print(self.ballast_bonus_input_wid.text_input_wid.text,
-              self.aps_hire_rate_input_wid.text_input_wid.text)
-        self.dismiss()
-
 
 class VSLPopup(Popup):
     rob_input_wid = ObjectProperty(None)
@@ -29,33 +24,9 @@ class VSLPopup(Popup):
     speed_input_wid = ObjectProperty(None)
     consumption_input_wid = ObjectProperty(None)
 
-    def Close(self):
-        print(self.rob_input_wid.text_input_wid.text,
-              self.price_input_wid.text_input_wid.text,
-              self.speed_input_wid.text_input_wid.text,
-              self.consumption_input_wid.text_input_wid.text)
-        self.dismiss()
-
 
 class ListElement(BoxLayout):
     description_wid = ObjectProperty(None)
-
-    # Add could move into CalculatorApp-class
-    def Add(self, app, element_type_selected):
-        if app.line_number < 9:
-            self.description_wid.text = element_type_selected
-            pad = app.root.ids.list.children[0]
-            app.root.ids.list.remove_widget(pad)
-            app.root.ids.list.add_widget(self)
-            app.root.ids.list.add_widget(Widget())
-            app.line_number += 1
-
-    # Remove cannot move into CalculatorApp-class,
-    # reference by instance to itself will be lost
-    # !!! or maybe just do: app.Remove(root) !!!
-    def Remove(self, app):
-        self.parent.remove_widget(self)
-        app.line_number -= 1
 
 
 class AddElementDropDown(DropDown):
@@ -71,11 +42,20 @@ class CalculatorApp(App):
     def CPPopupOpen(self):
         CPPopup().open()
 
+    def CPPopupClose(self, CPPopupInstance):
+        print(CPPopupInstance.ballast_bonus_input_wid.text_input_wid.text,
+              CPPopupInstance.aps_hire_rate_input_wid.text_input_wid.text)
+        CPPopupInstance.dismiss()
+
     def VSLPopupOpen(self):
         VSLPopup().open()
 
-    def ListElementAdd(self, element_type_selected):
-        ListElement().Add(self, element_type_selected)
+    def VSLPopupClose(self, VSLPopupInstance):
+        print(VSLPopupInstance.rob_input_wid.text_input_wid.text,
+              VSLPopupInstance.price_input_wid.text_input_wid.text,
+              VSLPopupInstance.speed_input_wid.text_input_wid.text,
+              VSLPopupInstance.consumption_input_wid.text_input_wid.text)
+        VSLPopupInstance.dismiss()
 
     def AddElementDropDownOpen(self, AddElementButton):
         DropDownInstance = AddElementDropDown()
@@ -85,6 +65,20 @@ class CalculatorApp(App):
                 self, 'element_type_selected', element_type_selected
             )
         )
+
+    def ListElementAdd(self, element_type_selected):
+        if self.line_number < 9:
+            list_widget = self.root.ids.list
+            ElementToAdd = ListElement()
+            ElementToAdd.description_wid.text = element_type_selected
+            list_widget.remove_widget(list_widget.children[0])
+            list_widget.add_widget(ElementToAdd)
+            list_widget.add_widget(Widget())
+            self.line_number += 1
+
+    def ListElementRemove(self, ElementToRemove):
+        ElementToRemove.parent.remove_widget(ElementToRemove)
+        self.line_number -= 1
 
     def Reset(self):
         self.root.ids.list.clear_widgets()
